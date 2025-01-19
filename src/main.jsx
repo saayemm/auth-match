d457f00aa057d7648844a1e51358f42ce2915291
Saayemm <sayemdcc@gmail.com>
2025-01-19 11:06:16 +0600

    route setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,29 @@ import App from './App.jsx'
 import ReactDOM from "react-dom/client";
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router';
+import Root from './components/root/Root.jsx';
+import Home from './components/Home.jsx';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>hello</div>
+    element: <Root></Root>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+    ]
   }
 ])
 
